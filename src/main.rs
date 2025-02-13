497ebf0ca52ea2ff530683376de25d11c5adb813
Will <47335043+williamfedele@users.noreply.github.com>
2025-02-13 15:53:53 -0500

    feat: file change time, ruff check

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use futures::future::join_all;
 use notify::event::ModifyKind;
 use notify::{recommended_watcher, Event, EventKind, RecursiveMode, Result, Watcher};
 use std::path::Path;
@@ -7,11 +6,12 @@ use std::path::PathBuf;
 use std::process::{Command, Output};
 use std::sync::mpsc;
 use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::time;
 use tokio::time::Instant;
 
 #[derive(Parser)]
-#[clap(name = "Mesa", version = "0.1.0", author = "")]
+#[clap(name = "Sentinel", version = "0.1.0", author = "")]
 struct Cli {
     #[arg(short, long, default_value = ".")] // default to current directory
     dir: String,
@@ -22,20 +22,21 @@ struct ToolConfig {
 }
 
 enum Tool {
-    Ruff,
+    RuffFormat,
+    RuffCheck,
 }
 
-struct Mesa {
+struct Sentinel {
     dir: String,
     tools: ToolConfig,
 }
 
-impl Mesa {
+impl Sentinel {
     fn new(dir: String) -> Result<Self> {
         Ok(Self {
             dir,
             tools: ToolConfig {
-                python_tools: vec![Tool::Ruff],
+                python_tools: vec![Tool::RuffFormat, Tool::RuffCheck],
             },
         })
     }
@@ -47,6 +48,7 @@ impl Mesa {
 
         let mut last_event: Option<(PathBuf, Instant)> = None;
 
+        println!("Watching for changes...");
         for res in rx {
             match res {
                 Ok(event) => match event.kind {
@@ -80,10 +82,21 @@ impl Mesa {
     async fn process_file(&self, path: &Path) -> Result<()> {
         match path.extension().and_then(|e| e.to_str()) {
             Some("py") => {
-                println!("Changed: {}", path.display());
+                // time and file that changed
+                let (hour, minute, second) = Self::get_current_time();
+                println!(
+                    "[{:0>2}:{:0>2}:{:0>2}] - File changed: {}",
+                    hour,
+                    minute,
+                    second,
+                    path.display()
+                );
                 let tools = &self.tools.python_tools;
-                let results = join_all(tools.iter().map(|tool| self.run_tool(tool, path))).await;
-                self.display_results(results);
+                // Execute tools sequentially
+                for tool in tools {
+                    let result = self.run_tool(tool, path).await;
+                    self.display_results(result); // Display each result immediately
+                }
                 Ok(())
             }
             _ => Ok(()),
@@ -92,11 +105,20 @@ impl Mesa {
 
     async fn run_tool(&self, tool: &Tool, path: &Path) -> Result<Output> {
         match tool {
-            Tool::Ruff => {
-                print!("Running ruff...");
+            Tool::RuffFormat => {
+                print!("Running ruff format...");
                 // get current time
                 let now = Instant::now();
                 let output = Command::new("ruff").arg("format").arg(path).output()?;
+                let elapsed = now.elapsed();
+                println!(" ✓ ({:.2?})", elapsed);
+                return Ok(output);
+            }
+            Tool::RuffCheck => {
+                print!("Running ruff check...");
+                // get current time
+                let now = Instant::now();
+                let output = Command::new("ruff").arg("check").arg(path).output()?;
                 let elapsed = now.elapsed();
                 println!(" ✓ ({:.2?})", elapsed);
                 return Ok(output);
@@ -105,21 +127,39 @@ impl Mesa {
         }
     }
 
-    fn display_results(&self, results: Vec<Result<Output>>) {
-        for result in results {
-            match result {
-                Ok(output) => {
-                    println!("{}", String::from_utf8_lossy(&output.stdout));
+    // get current time in HH:MM:SS format
+    fn get_current_time() -> (u64, u64, u64) {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+
+        let seconds = now.as_secs();
+
+        let second = seconds % 60;
+        let minute = (seconds % 3600) / 60;
+        let hour = (seconds / 3600) % 24;
+
+        return (hour, minute, second);
+    }
+
+    fn display_results(&self, results: Result<Output>) {
+        match results {
+            Ok(output) => {
+                if !output.stdout.is_empty() {
+                    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
                 }
-                Err(e) => println!("Error: {}", e),
+                if !output.stderr.is_empty() {
+                    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+                }
             }
+            Err(e) => println!("Error: {}", e),
         }
     }
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    let mesa = Mesa::new(cli.dir)?;
-    futures::executor::block_on(mesa.watch())?;
+    let sentinel = Sentinel::new(cli.dir)?;
+    futures::executor::block_on(sentinel.watch())?;
     Ok(())
 }
